f1e25fb1407dd3782e530caa107f9f4ac9725c6d
James <james.gong927@gmail.com>
2017-02-06 22:45:24 +1100

    Add weather class based functions

---
+++ extensions.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+
+class Weather(object):
+
+    def __init__(self):
+        return
+
+    def get_location(self):
+        pass
+
+    def get_weather(self):
+        pass
+
+    def send_messages(self):
+        pass
+
+    def run(self):
+        pass
---
+++ weather_reminder.py
@@ -0,0 +1,19 @@
+# need to run with nohup
+# e.g. `nohup python weather_reminder.py &`
+
+import schedule
+import time
+from extensions import Weather
+
+# def job(time):
+#     print (123, time)
+#     return
+
+
+# schedule.every().minute.do(job, 'varibale')
+#
+# while True:
+#     schedule.run_pending()
+#     time.sleep(60)
+
+
